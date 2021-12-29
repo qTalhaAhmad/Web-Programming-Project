0d6b9d92dfed146ffe2ed5bd6219e1e87f103b9b
dullfarhan <dullfarhan@gmail.com>
2021-12-30 01:44:32 +0500

    bootstrap working

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,57 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Navibar from './components/Navbar';
 
-class App extends Component {
-  render() {
-    return (
-      <Navibar/>
-     )
+import  NavComp  from './components/navbar';
+import CardComp from './components/card';
 
-  }
+
+
+
+
+function App() {
+  const infos = [
+    {
+      id: 1,
+      price: "300",
+      name: "Tops",
+      disp: "dsfjsidjfijivkhwktheruifhewuifhwfheruifhweivdfvn",
+    },
+    {
+      id: 2,
+      price: "300",
+      name: "Jewl",
+      disp: "dsfjsivdfjghdfjkg hhdkhwktheruifhewuifhwfheruifhweivdfvn",
+    },
+    {
+      id: 3,
+      price: "300",
+      name: "neck",
+      disp: "dsfjsidjfijivdfjghdfjkg hhdkhwktheruiruifhweivdfvn",
+    },
+    {
+      id: 4,
+      price: "300",
+      name: "shoes",
+      disp: "dsfjsidjfikhwktheruifhewuifhwfheruifhweivdfvn",
+    },
+    {
+      id: 5,
+      price: "300",
+      name: "caps",
+      disp: "dsfjsidjdfjghdfjkg hhdkhwktheruifhewuifhwfheruifhweivdfvn",
+    },
+  ];
+  return (
+    <div className="App">
+      <NavComp />
+      <div id="Body" className="body" style={{ marginLeft: "40px" }}>
+        <div style={{ display: "flex" }} id="itemsList" className="itemsList">
+          {infos.map((info) => (
+            <CardComp key={info.id} info={info} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
+
